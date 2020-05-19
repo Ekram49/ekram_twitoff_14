82660ab7c4dc8de42454feb3d6c424d165d08884
ekram49 <ekramullahzaki@gmail.com>
2020-05-19 16:36:17 -0400

    Bassilica service

---
+++ web_app/services/basilica_service.py
@@ -0,0 +1,28 @@
+#web_app/services/basilica_service.py
+
+from basilica import Connection
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASILICA_API_KEY = os.getenv("BASILICA_API_KEY", default="OOPS")
+
+connection = Connection(BASILICA_API_KEY)
+print(type(connection))
+
+embeddings = connection.embed_sentences("HELLO_WORLD")
+print(embeddings)
+
+sentences = [
+    "This is a sentence!",
+    "This is a similar sentence!",
+    "I don't think this sentence is very similar at all...",
+]
+
+embeddings = list(connection.embed_sentences(sentences))
+for embed in embeddings:
+    print("______________")
+    print(embed)
+
+
