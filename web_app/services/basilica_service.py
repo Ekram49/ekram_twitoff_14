09ea075041fbf8b910f7abf88162675a17601d76
ekram49 <ekramullahzaki@gmail.com>
2020-05-20 19:14:04 -0400

    app running ok

--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -1,20 +1,20 @@
-#web_app/services/basilica_service.py
+# web_app/services/basilica_service.py
 
 from basilica import Connection
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # get the contents of the .env file into the environment
 
 BASILICA_API_KEY = os.getenv("BASILICA_API_KEY", default="OOPS")
 
 connection = Connection(BASILICA_API_KEY)
 print(type(connection))
 
-if __name__ ==  "__main__":
+if __name__ == "__main__":
 
-    embeddings = connection.embed_sentences("HELLO_WORLD")
-    print(embeddings)
+    embedding = connection.embed_sentence("HELLO WORLD")
+    print(embedding) #>
 
     sentences = [
         "This is a sentence!",
@@ -24,7 +24,7 @@ if __name__ ==  "__main__":
 
     embeddings = list(connection.embed_sentences(sentences))
     for embed in embeddings:
-        print("______________")
-        print(embed)
+        print("----------")
+        print(embed) #> a list with 768 floats from -1 to 1
 
 
